7397a4eef08e6d562182a36d85853f58fbdf117d
이승연 <leelaf0002@gmail.com>
2025-08-21 18:01:29 +0900

    snake 클래스 구현중

--- program.cs
+++ program.cs
@@ -2,11 +2,15 @@
 Console.WriteLine();
 Console.WriteLine("SPACE 키를 눌러서 시작하세요!");
 //Console.WriteLine("SPACE 키를 눌러서 다시 시작하세요!");
-
 ConsoleKeyInfo keyInfo;
 keyInfo = Console.ReadKey();
 if (keyInfo.Key == ConsoleKey.Spacebar)
 {
     Console.Clear();
-    Map map = new Map(22, 22); // todo: 22 -> 20
+    Map map = new Map(22, 22);
+
+    Snake snake = new Snake();
+    snake.Init();
 }
+
+
--- snake.cs
+++ snake.cs
@@ -1,8 +1,15 @@
 ﻿public class Snake
 {
     Map map;
+    private int x { get; set; }
+    private int y { get; set; }
     public Snake()
     {
-        map.Active
+        x= 0; y = 0;
+    }
+
+    public void Init()
+    {
+        Console.Write("0");
     }
 }
